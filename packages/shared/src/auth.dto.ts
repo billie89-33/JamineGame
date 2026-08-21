7bcece5a3db481f3e54f67b3f08d8068aff45b77
billie89-33 <bowvorn@gmail.com>
2026-08-21 09:33:11 +0700

    feat: add user model and auth dtos

---
+++ packages/shared/src/auth.dto.ts
@@ -0,0 +1,11 @@
+﻿export class RegisterDto {
+  username!: string;
+  email!: string;
+  password!: string;
+  confirm!: string;
+}
+
+export class LoginDto {
+  username!: string;
+  password!: string;
+}

4b7c692a6829628d8c3c17fd1398155879c757bc
billie89-33 <bowvorn@gmail.com>
2026-08-21 11:57:04 +0700

    test: mock dependencies for all generated spec files to pass

--- packages/backend/src/modules/users/users.service.spec.ts
+++ packages/backend/src/modules/users/users.service.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
+    const mockPrismaService = {};
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: PrismaService, useValue: mockPrismaService }
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
